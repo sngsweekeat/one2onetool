5cc2bee681e27d89257da78dccb67c7d398f2a66
Sng Swee Keat <sng_swee_keat@ida.gov.sg>
2017-09-07 12:28:38 +0800

    Added handlebars templating and refactored routing

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// View enginer setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
+// Middleware setup
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routes
+app.use('/', routes);
+
+// Start server
 var port = process.env.PORT || '3000';
 app.set('port', port);
-
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
 });
