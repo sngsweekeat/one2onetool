2ec9c6b05ae3096dc762b5e5ac511d80f605fb5d
Sng Swee Keat <sng_swee_keat@ida.gov.sg>
2017-09-07 16:31:20 +0800

    Added data files, and generating category buttons

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const dataFolder = path.join(__dirname, '../data'); //TODO: better way to do this?
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: '1to1tool' });
+    var categories = getCategories();
+    console.log(categories);
+    res.render('index', {
+        title: '1to1tool',
+        categories: categories
+    });
 });
+
+function getCategories() {
+    console.log(dataFolder);
+    return fs.readdirSync(dataFolder);
+}
 
 module.exports = router;
