9bae543fb4c6f9dff48a4df6887d6acfb1cbeca4
Jayaprkash Singamsetty <jsingamsetty@csc.com>
2018-06-04 09:52:43 +0530

    udpates docker setup

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,6 +1,8 @@
-dockerBuild.sh
-
 #!/usr/bin/env bash
+sudo yum update -y
+sudo yum install -y docker  ## AWS ec2 Linux machine supports
+sudo service docker start
+sudo usermod -a -G docker ec2-user
 
 docker build --tag praqma/native-scons:latest --file ${PWD}/Dockerfile ${PWD}
 docker build -t new-prod-testing ${PWD}/Dockerfile
